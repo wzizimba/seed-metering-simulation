b93459a35c040f8954f16b7e7c2d30acdaf34212
Wezi Zimba <wzizimba@outlook.com>
2026-03-28 14:20:46 -0700

    Remove unused imports from kinematic.py

--- src/kinematic.py
+++ src/kinematic.py
@@ -1,6 +1,4 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from parameters import N_cells, C_w, ratio, Lambda, exp_spcng, expr_mean, sprocket_A, sproket_B
+from parameters import N_cells, C_w, ratio, Lambda
 
 def S_m(ratio: float=ratio) -> float:
     return (N_cells*ratio)/C_w
